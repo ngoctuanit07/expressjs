ae2fdd3a549225a052565bb5720e3577b1f9e4bf
Walter <walternguyen07@gmail.com>
2020-12-18 14:33:37 +0700

    add model Lead

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,14 @@
+const models = require('../models');
 exports.get_landing = function(req,res,next){
     res.render('index',{title:'Demo'});
 }
 exports.submit_form = function(req,res,next){
     console.log("email",req.body.lead_email);
+    return models.Lead.create({
+        email:req.body.lead_email
+    }).then(lead =>{
+        res.redirect('/');
+    })
    // alert("email",req.body.lead_email);
-    res.redirect('/');
+
 }
